43880cf498157ffe94d8f722a5788468f8d6ff06
Vinicius Fabri <vfabri@ciandt.com>
2019-02-12 09:52:49 -0200

    Adding missing API endpoints

--- src/Api/Currency.php
+++ src/Api/Currency.php
@@ -13,8 +13,24 @@ class Currency extends Master {
     // https://www.alphavantage.co/documentation/#currency-exchange
     const CURRENCY_EXCHANGE_RATE = 'CURRENCY_EXCHANGE_RATE';
 
+    // https://www.alphavantage.co/documentation/#fx-intraday
+    const FX_INTRADAY = 'FX_INTRADAY';
+
+    // https://www.alphavantage.co/documentation/#fx-daily
+    const FX_DAILY = 'FX_DAILY';
+
+    // https://www.alphavantage.co/documentation/#fx-weekly
+    const FX_WEEKLY = 'FX_WEEKLY';
+
+    // https://www.alphavantage.co/documentation/#fx-monthly
+    const FX_MONTHLY = 'FX_MONTHLY';
+
     private $availableFunctions = [
         self::CURRENCY_EXCHANGE_RATE,
+        self::FX_INTRADAY,
+        self::FX_DAILY,
+        self::FX_WEEKLY,
+        self::FX_MONTHLY,
     ];
 
     /**
@@ -29,6 +45,68 @@ class Currency extends Master {
         return $this->query(self::CURRENCY_EXCHANGE_RATE, array_merge($params, [
             'from_currency' => $from,
             'to_currency' => $to,
+        ]));
+    }
+
+    /**
+     * FX_INTRADAY
+     * @param  string $from      The currency you would like to get the exchange rate for.
+     * @param  string $to        The destination currency for the exchange rate.
+     * @param  string $interval  One of: 1min, 5min, 15min, 30min, 60min.
+     * @param  string $params    Additional params
+     * @return Object            Response object
+     */
+    public function intraday($from, $to, $interval, $params = [])
+    {
+        return $this->query(self::FX_INTRADAY, array_merge($params, [
+            'from_symbol' => $from,
+            'to_symbol' => $to,
+            'interval' => $interval,
+        ]));
+    }
+
+    /**
+     * FX_DAILY
+     * @param  string $from      The currency you would like to get the exchange rate for.
+     * @param  string $to        The destination currency for the exchange rate.
+     * @param  string $params    Additional params
+     * @return Object            Response object
+     */
+    public function daily($from, $to, $params = [])
+    {
+        return $this->query(self::FX_DAILY, array_merge($params, [
+            'from_symbol' => $from,
+            'to_symbol' => $to,
+        ]));
+    }
+
+    /**
+     * FX_WEEKLY
+     * @param  string $from      The currency you would like to get the exchange rate for.
+     * @param  string $to        The destination currency for the exchange rate.
+     * @param  string $params    Additional params
+     * @return Object            Response object
+     */
+    public function weekly($from, $to, $params = [])
+    {
+        return $this->query(self::FX_WEEKLY, array_merge($params, [
+            'from_symbol' => $from,
+            'to_symbol' => $to,
+        ]));
+    }
+
+    /**
+     * FX_MONTHLY
+     * @param  string $from      The currency you would like to get the exchange rate for.
+     * @param  string $to        The destination currency for the exchange rate.
+     * @param  string $params    Additional params
+     * @return Object            Response object
+     */
+    public function monthly($from, $to, $params = [])
+    {
+        return $this->query(self::FX_MONTHLY, array_merge($params, [
+            'from_symbol' => $from,
+            'to_symbol' => $to,
         ]));
     }
 
